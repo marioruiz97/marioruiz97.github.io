5d0a59ad8d66487059ae830c4449ee5f42592d00
Mario Ruiz <marioarb.97@gmail.com>
2016-07-22 01:12:24 -0500

    Estructura archivo HTML
    Estilos CSS para secciones.
    Etiquetas semánticas para secciones.
    Archivo JS sin implementar. Para asignar dinámicamente anchos y altos a
    cursos y certificados.

--- Web/js/main.js
+++ Web/js/main.js
@@ -1,1 +1,34 @@
+function size(){
 
+	var caja, padre;
+
+	caja= document.getElementsByClassName('curso');
+	padre=caja.parentElement;
+
+	var ancho = getWidth(padre);
+	ancho = ancho * .9;
+	var alto = ancho * .75;
+
+	setStyles(ancho, alto);
+
+
+	var uno, dos;
+	uno = getWidth(caja);
+	dos = getHeight(caja);
+	console.log(uno + " " + dos);
+}
+
+function getWidth(elemento) {
+	return elemento.clientWidth;
+}
+
+function getHeight(elemento){
+	return elemento.clientWidth;
+}
+
+function setStyles(ancho, alto){
+	var elemento = document.getElementsByClassName('curso');
+
+	elemento.style.Width = ancho;
+	elemento.style.Height = alto;
+}
