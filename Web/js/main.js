280b12444acf6e78ced6365c01ec2a301abfb9ee
Mario Ruiz <Marioarb.97@gmail.com>
2016-07-25 00:07:33 -0500

    Actualización página web
    Falta:
    - Cursos y certificaciones.
    - minificar css
    - corregir error en el main.js

--- Web/js/main.js
+++ Web/js/main.js
@@ -1,3 +1,41 @@
+//Definir el alto del background del header al tamaño del viewport
+      var vHeight = $(window).height(),
+      cover = $('.cover');
+
+      cover.css({"height":vHeight,"width":'100%'});
+
+      //Obtener el año actual con JQuery
+      var anio = (new Date).getFullYear();
+ 
+      $(document).ready(function() {
+      $(".fecha").text( anio );
+      });
+
+
+//Código para el botón de volver arriba
+$(document).ready(function(){
+
+	$('.back-top').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 500);
+	});
+
+	//aparecer botón en scroll
+	$(window).scroll(function(){
+		if ( $(this).scrollTop() > 0 ){
+			$('.back-top').slideDown(500);
+		}
+		else{
+			$('.back-top').slideUp(500);
+		}
+	});
+});
+
+
+
+
+
 function size(){
 
 	var caja, padre;
@@ -32,3 +70,7 @@ function setStyles(ancho, alto){
 	elemento.style.Width = ancho;
 	elemento.style.Height = alto;
 }
+
+
+
+
